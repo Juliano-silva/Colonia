8a9afbab0bbe319a91a96671697f0671753b8835
Juliano Almeida <88514570+Juliano-silva@users.noreply.github.com>
2021-09-13 09:24:13 -0300

    13/09/2021

--- Java.js
+++ Java.js
@@ -1,12 +1,46 @@
 const chat = document.getElementById("chat");
 function load(){
-  const novoH1 =  "<p id='play'>Numberlessama"+ "<span>:kkkkkkk</span>";
-  const novoH2 =  "<p id='play'>Numberlessama2"+ "<span>:kkkkkkkk</span>";
-  chat.insertAdjacentHTML("beforeend",novoH1)
+  const newH1 =  "<p id='play'>Numberlessama"+ "<span>:kkkkkkk</span>";
+  const newH2 =  "<p id='play2'>PeroxJukal"+ "<span>:F no chat</span>";
+  const newH3 =  "<p id='play3'>Harleyoff"+ "<span>:F no chat</span>";
+  const newH4 =  "<p id='play4'>Tikibou"+ "<span>:F no chat</span>";
+  const newH5 =  "<p id='play5'>VanSable"+ "<span>:F no chat</span>";
+  const newH6 =  "<p id='play6'>Wh1dyC0d4l"+ "<span>:F no chat</span>";
+  const newH7 =  "<p id='play7'>Charcoaloff"+ "<span>:F no chat</span>";
+  const newH8 =  "<p id='play'>VonKaput"+ "<span>:F no chat</span>";
+  const newH9 =  "<p id='play6'>Xaesqueak"+ "<span>:F no chat</span>";
+  const newH10 =  "<p id='play8'>TheSpiffyLamentable"+ "<span>:F no chat</span>";
+  const newH11 =  "<p id='play9'>Berserkstein"+ "<span>:F no chat</span>";
+  const newH12 =  "<p id='play7'>VigorousSnort"+ "<span>:F no chat</span>";
+  const newH13 =  "<p id='play4'>MacabreBuzz"+ "<span>:F no chat</span>";
+  const newH14 =  "<p id='play6'>Flowerpotsa"+ "<span>:F no chat</span>";
+  const newH15 =  "<p id='play10'>DasDeafening"+ "<span>:F no chat</span>";
+  chat.insertAdjacentHTML("beforeend",newH1)
+  chat.insertAdjacentHTML("beforeend",newH2)
+  chat.insertAdjacentHTML("beforeend",newH2)
+  chat.insertAdjacentHTML("beforeend",newH2)
   setTimeout(function(){
-    chat.insertAdjacentHTML("beforeend",novoH2)
-  },1000 )
-
+    chat.insertAdjacentHTML("beforeend",newH3)
+  },2000)
+  chat.insertAdjacentHTML("beforeend",newH2)
+  chat.insertAdjacentHTML("beforeend",newH2)
+  chat.insertAdjacentHTML("beforeend",newH4)
+  chat.insertAdjacentHTML("beforeend",newH4)
+  chat.insertAdjacentHTML("beforeend",newH5)
+  chat.insertAdjacentHTML("beforeend",newH5)
+  chat.insertAdjacentHTML("beforeend",newH5)
+  chat.insertAdjacentHTML("beforeend",newH6)
+  chat.insertAdjacentHTML("beforeend",newH6)
+  chat.insertAdjacentHTML("beforeend",newH7)
+  chat.insertAdjacentHTML("beforeend",newH8)
+  chat.insertAdjacentHTML("beforeend",newH9)
+  chat.insertAdjacentHTML("beforeend",newH9)
+  chat.insertAdjacentHTML("beforeend",newH10)
+  chat.insertAdjacentHTML("beforeend",newH11)
+  chat.insertAdjacentHTML("beforeend",newH12)
+  chat.insertAdjacentHTML("beforeend",newH13)
+  chat.insertAdjacentHTML("beforeend",newH14)
+  chat.insertAdjacentHTML("beforeend",newH15)
 }
-
-button.addEventListener("loadstart",load);
+await(chat.insertAdjacentHTML("beforeend",h1))
+addEventListener("loadstart",load);
