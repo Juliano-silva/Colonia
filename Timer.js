f7cd66b2370aff892777925feaba96e4aa5bb8a6
Juliano Almeida <88514570+Juliano-silva@users.noreply.github.com>
2021-09-19 13:48:33 -0300

    Timer

---
+++ Timer.js
@@ -0,0 +1,25 @@
+"use strict"
+var hh =0;
+var mm =0;
+var ss =0;
+
+var tempo = 1000;
+var cron;
+
+function load(){
+    cron = setInterval(() => {timer();}, tempo);
+}
+function timer(){
+    ss++;
+    if (ss==60){
+        ss = 0;
+        mm++;
+        
+        if(mm == 60){
+            mm = 0;
+            hh++;
+        }
+    }
+    var format = (hh <10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss) ;
+    document.getElementById('horas').innerText = format;
+}
