59fa7fa8dfa9d91dd8d2b0ecf22a8f0398827bb6
Juliano Almeida <88514570+Juliano-silva@users.noreply.github.com>
2021-09-24 20:46:21 -0300

    24/09/2021

--- Ocultar.js
+++ Ocultar.js
@@ -1,12 +1,26 @@
 const you = document.getElementById("You");
+const ocultar =  document.getElementById("ocultar");
+const iframe = document.getElementById("iframe")
 var i=0;
 function ocult(){
    if(!i){
-       you.style.display="none";
+       you.style.display="flex";
+       you.style.marginTop="-1000px";
+       iframe.style.width= "1575px";
+       iframe.style.display="block";
+       iframe.style.margin="auto"
+       iframe.style.marginRight="350px"
+       iframe.style.marginTop="-300px"
        i=1;
     }
     else{
-        you.style.display="inline";
+        you.style.display="inline"
+        you.style.marginTop="-5px";
+        iframe.style.width= "1430px";
+       iframe.style.display="block";
+       iframe.style.margin="auto"
+       iframe.style.marginRight="550px"
+       iframe.style.marginTop="-300px"
         i=0;
     }
 }
