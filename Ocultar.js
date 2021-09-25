fb4837996b842e25e413ecadae0e825b09fcbd4e
Juliano Almeida <88514570+Juliano-silva@users.noreply.github.com>
2021-09-25 02:24:39 -0300

    Eu ocultei o ocultado

--- Ocultar.js
+++ Ocultar.js
@@ -1,6 +1,11 @@
 const you = document.getElementById("You");
 const ocultar =  document.getElementById("ocultar");
-const iframe = document.getElementById("iframe")
+const iframe = document.getElementById("iframe");
+const ch = document.getElementById("chat");
+const oc = document.getElementById("ocultarC")
+const txt = document.getElementById("textarea");
+const fav = document.getElementById("favi");
+const en = document.getElementById("enter");
 var i=0;
 function ocult(){
    if(!i){
@@ -22,5 +27,38 @@ function ocult(){
        iframe.style.marginRight="550px"
        iframe.style.marginTop="-300px"
         i=0;
+    }
+}
+var i=0;
+function ocultC(){
+    if(!i){
+        ch.style.display="flex";
+        ch.style.marginTop="-1000px";
+        txt.style.display="none"
+        fav.style.display="none"
+        en.style.display="none"
+        oc.style.marginLeft="215px"
+        oc.style.marginTop="-190px"
+        iframe.style.width= "1650px";
+       iframe.style.display="block";
+       iframe.style.margin="auto"
+       iframe.style.marginRight="350px"
+       iframe.style.marginTop="-100px"
+        i=1
+    }
+    else{
+        ch.style.display="inline";
+        ch.style.marginTop="-710px";
+        txt.style.display="inline"
+        fav.style.display="inline"
+        en.style.display="inline"
+        oc.style.marginLeft="-1px"
+        oc.style.marginTop="2px"
+        iframe.style.width= "1430px";
+       iframe.style.display="block";
+       iframe.style.margin="auto"
+       iframe.style.marginRight="265px"
+       iframe.style.marginTop="-100px"
+        i=0
     }
 }
